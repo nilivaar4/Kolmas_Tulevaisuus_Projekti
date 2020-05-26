387097b478c60405db726f00c14b29d925dfbc2c
nilivaar4 <nikolasnilivaara@msn.com>
2020-05-26 03:32:33 +0300

    Placeholder juttui + kamera
    Lisätty peliin alustava background, joka toimii tavallaan pelin "karttana". Myös kamera toimii jotenkin alustavasti. Canvas saatu hyvin backgroundiin kiinni.

---
+++ Assets/C#/tietokoneKoodi.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class tietokoneKoodi : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Debugg()
+    {
+        Debug.Log("Hello World!");
+    }
+
+}
