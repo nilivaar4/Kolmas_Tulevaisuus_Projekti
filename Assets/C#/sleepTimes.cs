5052893f166a34c4f87dc23718966d69157cb636
nilivaar4 <nikolasnilivaara@msn.com>
2020-06-23 01:43:46 +0300

    "Aikapisteet" + Nukkuminen, UI
    Tehty pelimekaniikka "Aikapisteille", luotu mahdollisuus nukkua, joka vaikuttaa hyvinvointiin, yms, muokattu ulkoasua.

---
+++ Assets/C#/sleepTimes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sleepTimes : MonoBehaviour
+{
+
+    public GameObject stress, sleep;
+
+    public void SleepShort()
+    {
+        stress.GetComponent<stressAmount>().subtractHealth(10);
+        sleep.GetComponent<timePoints>().subtracthours(180);
+    }
+
+    public void SleepMedium()
+    {
+        sleep.GetComponent<timePoints>().subtracthours(240);
+    }
+
+    public void SleepLong()
+    {
+        stress.GetComponent<stressAmount>().addHealth(10);
+        sleep.GetComponent<timePoints>().subtracthours(300);
+    }
+}
---
+++ Assets/C#/timePoints.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timePoints : MonoBehaviour
+{
+    public Text hoursText;
+    int hours;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hours = 720;
+        hoursText.text = hours.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hoursText.text = hours.ToString() + " tuntia käytössä";
+        Limitations();
+    }
+
+    //Add money to player
+    public void addhours(int hoursToAdd)
+    {
+        hours += hoursToAdd;
+    }
+
+    //Subtract players money
+    public void subtracthours(int hoursToSubtract)
+    {
+        hours -= hoursToSubtract;
+    }
+
+    public void Limitations()
+    {
+        if (hours <= 0)
+        {
+            hoursText.text = "0 Tuntia käytössä";
+            hours = 0;
+        }
+        else if (hours >= 0)
+        {
+            hoursText.text = hours.ToString() + " tuntia käytössä";
+
+        }
+    }
+}
